a8f8517d75612149a13fb0112a1ccbe58899c9bd
AvenCores <avencores@gmail.com>
2022-12-04 18:03:09 +0300

    V2.0

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from os import system,name
 from time import sleep
 
 while True:
-    server = JavaServer.lookup("play.leadertv.ru")
-    status = server.status()
-    query = server.query()
-    system('cls' if name == 'nt' else 'clear')
-    print(colored("Сервер - ", "red") + ("play.leadertv.ru\n") + colored("Версия - ", "cyan") + ("1.19.2\n"))
-    print(colored(f"На сервере онлайн {status.players.online} игрока(ов)", "yellow"))
-    print(colored(f"На сервере онлайн следующие игроки: {', '.join(query.players.names)}", "blue"))
-    sleep(5)
+    try:
+        server = JavaServer.lookup("game.leadertv.ru")
+        status = server.status()
+        query = server.query()
+        system('cls' if name == 'nt' else 'clear')
+        print(colored("Сервер - ", "red") + ("play.leadertv.ru\n") + colored("Версия - ", "cyan") + ("1.19.2\n"))
+        print(colored(f"На сервере онлайн {status.players.online} игрока(ов)", "yellow"))
+        print(colored(f"На сервере онлайн следующие игроки: {', '.join(query.players.names)}", "blue"))
+        sleep(5)
+    except:
+        system('cls' if name == 'nt' else 'clear')
+        print(colored("Не удалось подключиться к серверу!", "red") + colored("\n\nПоторяю попытку...", "yellow"))
+        sleep(2)
